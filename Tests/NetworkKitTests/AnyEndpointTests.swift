e6e00c6fd158c6727d3ebab3d5b7c54a35eabc08
Breno Marques <brenomarques516@gmail.com>
2026-03-17 15:39:25 -0300

    :test_tube: test: set files for testing

--- Tests/NetworkKitTests/AnyEndpointTests.swift
+++ Tests/NetworkKitTests/AnyEndpointTests.swift
@@ -1,3 +1,10 @@
+//
+//  AnyEndpointTests.swift
+//  NetworkKit
+//
+//  Created by Breno Marques on 17/03/26.
+//
+
 import Testing
 @testable import NetworkKit
 
---
+++ Tests/NetworkKitTests/NetworkEndpointTests.swift
@@ -0,0 +1,9 @@
+//
+//  NetworkEndpointTests.swift
+//  NetworkKit
+//
+//  Created by Breno Marques on 17/03/26.
+//
+
+import Testing
+@testable import NetworkKit
